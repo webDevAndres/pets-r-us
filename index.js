f72c1a6ce23ba0dc8b000f6fcf1612adbf8cb8e7
Andres Macias <macias.andres02@gmail.com>
2022-06-28 15:45:35 -0500

    add update to file structure

---
+++ index.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let path = require('path');
+let http = require('http');
+
+let app = express();
+
+//routes to the views folder
+let viewsPath = path.resolve(__dirname, 'views');
+app.use(express.static(viewsPath));
+
+// routes to public folder
+let publicPath = path.resolve(__dirname,'public');
+app.use(express.static('public'));
+
+
+
+
+
+
+
+http.createServer(app).listen(3000);
